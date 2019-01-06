0ce71c4da70fa77f975252d37bab8efbbf6f02c6
Chunpeng Huo <huocp@me.com>
2019-01-07 10:31:31 +1100

    fix: make runtime-composition-aurelia gulp task runnable on windows

--- runtime-composition-aurelia/gulpfile.js
+++ runtime-composition-aurelia/gulpfile.js
@@ -3,19 +3,19 @@ var run = require('gulp-run');
 
 function buildSharedUI() {
   console.info('Building shared-ui');
-  return run('cd shared-ui; npm i; npx gulp clean; npx gulp build-plugin').exec();
+  return run('cd shared-ui && npm i && npx gulp clean && npx gulp build-plugin').exec();
 }
 
 function buildPart(proj) {
   return () => {
     console.info('Building ' + proj);
-    return run(`cd ${proj}; npm i; npx gulp clean; npx gulp build`).exec();
+    return run(`cd ${proj} && npm i && npx gulp clean && npx gulp build`).exec();
   };
 }
 
 function runHostApp() {
   console.info('Starting host-app');
-  return run('cd host-app; npm i; npx gulp run').exec();
+  return run('cd host-app && npm i && npx gulp run').exec();
 }
 
 exports.default = gulp.series(
